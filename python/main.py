7f9ad3210f006a3b6ccd187bba4bc6f4e37d9761
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-07-16 17:12:53 +0900

    Fixed data receiver's cui.

--- python/main.py
+++ python/main.py
@@ -14,11 +14,14 @@ while True:
     bytes = ser.read(data_num)
     if len(bytes) == data_num:
         ax, ay, az, gx, gy, gz, mx, my, mz = struct.unpack('>fffffffff', bytes)
-        print('accel(x:%f y:%f z:%f) ' % (ax, ay, az), end="")
-        print('gyro(x:%f y:%f z:%f) ' % (gx, gy, gz), end="")
-        print('mag(x:%f y:%f z:%f)' % (mx, my, mz))
+        msg = ''
+        msg += 'accel(x:% 9.5f y:% 9.5f z:% 9.5f)  ' % (ax, ay, az)
+        msg += 'gyro(x:% 9.5f y:% 9.5f z:% 9.5f)  ' % (gx, gy, gz)
+        msg += 'mag(x:% 10.5f y:% 10.5f z:% 10.5f)' % (mx, my, mz)
     else:
-        print("no data available.")
+        msg = "no data available."
+    sys.stdout.write('\r\033[K' + msg)
+    sys.stdout.flush()
 
 
 ser.close()

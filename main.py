c0342a1476b16940601c0cad8fd7125b0270d23c
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-07-16 19:13:23 +0900

    modify cui. 	- select device by keyboard.

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 #!/usr/bin/env python
 
+from sys import stdout
 from data_receiver import DataReceiver
 from optparse import OptionParser
-import sys
+import glob
+from sys import platform
+import serial
+
+
+def get_available_serial_devices():
+    if platform.startswith('win'):
+        ports = ['COM' + str(i + 1) for i in range(256)]
+    elif platform.startswith('linux') or platform.startswith('cygwin'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+def select_serial_device():
+    devices = get_available_serial_devices()
+    while True:
+        for i in range(len(devices)):
+            print('%2d: %s' % (i, devices[i]))
+        key_input = input('please select device: ')
+        try:
+            di = int(key_input)
+            if di < len(devices):
+                break
+        except:
+            pass
+    return devices[di]
 
 if __name__ == '__main__':
     parser = OptionParser()
     (opts, args) = parser.parse_args()
 
-    if len(args) < 1:
-        exit()
-
-    serial_device = args[0]
     data_receiver = DataReceiver()
+    if len(args) < 1:
+        serial_device = select_serial_device()
+    else:
+        serial_device = args[0]
+
     data_receiver.set_serial_settings(serial_device)
 
     while True:
@@ -25,5 +65,5 @@ if __name__ == '__main__':
         except Exception as e:
             msg = "no data available."
         finally:
-            sys.stdout.write('\r\033[K' + msg)
-            sys.stdout.flush()
+            stdout.write('\r\033[K' + msg)
+            stdout.flush()

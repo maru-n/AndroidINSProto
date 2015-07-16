bc4d2e7b99f1bc91ecc46ce36014fa8efed48db5
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2015-07-16 15:34:19 +0900

    fixed python code for request-response type.

--- python/main.py
+++ python/main.py
@@ -6,14 +6,19 @@ import sys
 
 serial_device = sys.argv[1]
 baudrate = 115200
-ser = serial.Serial(serial_device, baudrate)
+ser = serial.Serial(serial_device, baudrate, timeout=0.1)
 
 while True:
-    data_num = 12
+    ser.write([1])
+    data_num = 12*3
     bytes = ser.read(data_num)
-    if len(bytes) != data_num:
-        Exception("Couldn't read all serial datas.")
-    x, y, z = struct.unpack('>fff', bytes)
-    print('x:%f y:%f z:%f' % (x, y, z))
+    if len(bytes) == data_num:
+        ax, ay, az, gx, gy, gz, mx, my, mz = struct.unpack('>fffffffff', bytes)
+        print('accel(x:%f y:%f z:%f) ' % (ax, ay, az), end="")
+        print('gyro(x:%f y:%f z:%f) ' % (gx, gy, gz), end="")
+        print('mag(x:%f y:%f z:%f)' % (mx, my, mz))
+    else:
+        print("no data available.")
+
 
 ser.close()
